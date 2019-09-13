ea0da4a3741aeefa9db263305cb3fb17317fec65
Guilherme Vieira <guilherme@feracode.com>
2019-09-13 19:37:10 -0300

    Grant logic implemented

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import crypto from 'crypto'
+import moment from 'moment'
+import axios from 'axios'
+
 export default class {
 	constructor(app) {
 		this.app = app
 
 		app.get('/register-application', this.registerApplicationHandler.bind(this))
-		app.get('/authorize-frontend')
+		app.post('/authorize', this.authorization.bind(this))
+		app.post('/token', this.token.bind(this))
 	}
 
 	async registerApplicationHandler(req, res, next) {
+		//TODO Need to be authenticated
 		await this.registerApplication({
 			name: 'New Used Media',
 			logo: 'https://dev.newusedmedia.com/static/media/logo_white.e0ee2117.png',
@@ -18,9 +23,80 @@ export default class {
 		res.send('Success!')
 	}
 
-	async token(req, res, next) {
-		console.log('chamou')
+	async authorization(req, res, next) {
+		let { client_id, scope, state, response_type, redirect_uri } = req.body
+		let userId = 'user123' //TODO Get from somewhere cookies or auth headers, this can go to the example app
+
+		let application = await this.getApplication(client_id)
+		if (!application) {
+			return res.status(400).send('Client not found')
+		}
+
+		let user = await this.getUser(userId)
+		if (!user) {
+			return res.status(400).send('User not found')
+		}
+
+		if (application.redirectURI !== redirect_uri) {
+			return res.status(400).send('Redirect URI mismatch!')
+		}
+		if (response_type === 'code') {
+			let code = await this.generateAuthorizationCode({ userId, clientId: application.clientId, scope })
+
+			return res.redirect(application.redirectURI + '?' + serialize({ code, state }))
+		} else {
+			return res.status(400).redirect(application.redirectURI + '?error=Response type not found')
+		}
 	}
+
+	async generateAuthorizationCode({ clientId, userId, scope }) {
+		let code = 'smash_authorization_code_' + crypto.randomBytes(10).toString('hex')
+
+		this.saveAuthorizationCode({ code, clientId, userId, scope })
+		return code
+	}
+
+	async generateAccessToken({ application, clientId, scope }) {
+		let accessToken = 'smash_access_token_' + crypto.randomBytes(10).toString('hex')
+		let accessTokenExpiresOn = moment().add(1, 'hour')
+		let refreshToken = 'smash_refresh_token_' + crypto.randomBytes(10).toString('hex')
+		let refreshTokenExpiresOn = moment().add(30, 'days')
+		let userId = application.userId
+
+		return this.saveAccessToken({
+			accessToken,
+			accessTokenExpiresOn,
+			refreshToken,
+			refreshTokenExpiresOn,
+			clientId,
+			userId,
+			scope
+		})
+	}
+
+	async token(req, res, next) {
+		let { grant_type, code, redirect_uri, client_id, client_secret } = req.body
+
+		let application = await this.getApplication(client_id)
+		if (!application) {
+			return res.status(400).send('Client not found')
+		}
+
+		if (application.clientSecret !== client_secret) {
+			return res.status(400).send('Secret mismatch')
+		}
+
+		let authorizationCode = await this.getAuthorizationCode(code)
+		if (!authorizationCode) {
+			return res.status(400).send('Authorization code not found')
+		}
+
+		if (grant_type === 'authorization_code') {
+			let accessToken = await this.generateAccessToken({ application, clientId: client_id })
+			return res.send(accessToken)
+		}
+	}
+
 	/**
 	 * @param {Object} application - The employee who is responsible for the project.
 	 * @param {string} application.name - The name of the employee.
@@ -40,16 +116,8 @@ export default class {
 
 	verifyIfRedirectUriIsValid(redirectURI) {
 		if (false) {
-			throw new Error('Must implement error verification') //TODO implementar
+			throw new Error('Must implement error verification') //TODO implementar maybe not 404 on get
 		}
-	}
-
-	async saveApplication({ name, website, logo, redirectURI, userId, clientID, clientSecret }) {
-		throw new Error('Must implement')
-	}
-
-	async renderAuthorizationView() {
-		throw new Error('Must implement')
 	}
 
 	initViews() {
@@ -60,9 +128,61 @@ export default class {
 	}
 
 	async authenticatedMiddleware(req, res, next) {
-		if (req.query.logged === 'yes') {
+		if (req.query.logged !== 'yes') {
 			return next()
 		}
 		res.status(400).send('Not authenticated')
 	}
+
+	// Must implement
+
+	async saveApplication({ name, website, logo, redirectURI, userId, clientId, clientSecret }) {
+		throw new Error('Must implement')
+	}
+
+	async getApplication(clientID) {
+		throw new Error('Must implement')
+	}
+
+	async saveAccessToken({
+		accessToken,
+		accessTokenExpiresOn,
+		refreshToken,
+		refreshTokenExpiresOn,
+		clientId,
+		userId,
+		scope
+	}) {
+		throw new Error('Must implement')
+	}
+
+	async saveAuthorizationCode({ code, clientId, userId, scope }) {
+		throw new Error('Must implement')
+	}
+
+	async getAuthorizationCode(code) {
+		throw new Error('Must implement')
+	}
+
+	async renderAuthorizationView() {
+		throw new Error('Must implement')
+	}
+
+	async getDevUser(devUserId) {
+		throw new Error('Must implement')
+	}
+
+	async getUser(userId) {
+		throw new Error('Must implement')
+	}
+}
+
+function serialize(obj) {
+	var str = []
+	for (var p in obj) {
+		if (obj.hasOwnProperty(p)) {
+			str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]))
+		}
+	}
+	return str.join('&')
 }
